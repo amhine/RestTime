59a16bc99edb642fe53012b50d6dccc26eab4d36
amhine <nihadamhine@gmail.com>
2025-12-17 21:12:53 +0100

    Initial commit

---
+++ src/main/java/com/RestTime/RestTime/RestTimeApplication.java
@@ -0,0 +1,13 @@
+package com.RestTime.RestTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RestTime/RestTime/RestTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestTime.RestTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
