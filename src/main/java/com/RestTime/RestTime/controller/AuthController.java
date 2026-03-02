ff28b9d3c55320384b20a98d20b8110075eed950
amhine <nihadamhine@gmail.com>
2026-03-02 14:37:28 +0000

    renitialisation de mots de passe

--- src/main/java/com/RestTime/RestTime/controller/AuthController.java
+++ src/main/java/com/RestTime/RestTime/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.RestTime.RestTime.controller;
 
 import com.RestTime.RestTime.dto.AuthResponse;
+import com.RestTime.RestTime.dto.ForgotPasswordRequestDTO;
 import com.RestTime.RestTime.dto.LoginRequest;
 import com.RestTime.RestTime.service.AuthService;
+import com.RestTime.RestTime.service.UtilisateurService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-
+    private final UtilisateurService utilisateurService;
 
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<String> forgotPassword(@RequestBody ForgotPasswordRequestDTO request) {
+        utilisateurService.requestPasswordReset(request);
+        return ResponseEntity.ok("Si l'email existe, un lien de réinitialisation a été envoyé.");
     }
 }
