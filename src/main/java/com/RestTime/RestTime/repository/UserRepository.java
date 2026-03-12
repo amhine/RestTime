a0f45e3fb68c94ca5abeb8318aa01a892ef350a8
amhine <nihadamhine@gmail.com>
2026-03-12 14:09:49 +0000

    update HistoriqueRepository

--- src/main/java/com/RestTime/RestTime/repository/UserRepository.java
+++ src/main/java/com/RestTime/RestTime/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.RestTime.RestTime.repository;
 
-import com.RestTime.RestTime.model.entity.Utilisateur;
+import com.RestTime.RestTime.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-public interface UserRepository extends JpaRepository<Utilisateur, Long> {
-    Optional<Utilisateur> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
 }
