9799cebb8b3bd3e839c98f604fc4cfefc175a098
amhine <nihadamhine@gmail.com>
2026-03-13 01:21:09 +0000

    add employe

--- src/main/java/com/RestTime/RestTime/controller/EmployeController.java
+++ src/main/java/com/RestTime/RestTime/controller/EmployeController.java
@@ -1,12 +1,17 @@
 package com.RestTime.RestTime.controller;
 
+import com.RestTime.RestTime.dto.DemandeCongeCreateDTO;
+import com.RestTime.RestTime.dto.DemandeCongeResponseDTO;
 import com.RestTime.RestTime.dto.JustificatifResponseDTO;
 import com.RestTime.RestTime.dto.SoldeCongeDTO;
+import com.RestTime.RestTime.service.CongeService;
 import com.RestTime.RestTime.service.EmployeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employes")
@@ -37,5 +42,17 @@ public class EmployeController {
             @PathVariable Long demandeId,
             @RequestParam("fichier") MultipartFile fichier) {
         return ResponseEntity.ok(employeService.uploadJustificatif(demandeId, userId, fichier));
+    }
+
+    private final CongeService congeService;
+
+    @PostMapping("/{userId}/soumettre")
+    public ResponseEntity<DemandeCongeResponseDTO> soumettre(@PathVariable Long userId, @RequestBody DemandeCongeCreateDTO dto) {
+        return ResponseEntity.ok(congeService.soumettreDemande(userId, dto));
+    }
+
+    @GetMapping("/{userId}/mesdemandes")
+    public ResponseEntity<List<DemandeCongeResponseDTO>> getMesDemandes(@PathVariable Long userId) {
+        return ResponseEntity.ok(congeService.getMesDemandes(userId));
     }
 }
